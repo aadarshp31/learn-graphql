ac4600842bf022eb6a62dd9cc3954cb012eecf14
Adarsh Pandey <44257152+aadarshp31@users.noreply.github.com>
2023-10-25 18:10:35 +0530

    add resolvers for root Query type def

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,30 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+// data db
+import db from "./data/_db";
+
 // type definitions
 import { typeDefs } from "./schema";
+
+const resolvers = {
+    Query: {
+        games() {
+            return db.games;
+        },
+        authors() {
+            return db.authors;
+        },
+        reviews() {
+            return db.reviews;
+        }
+    }
+}
 
 // server setup
 const server = new ApolloServer({
     typeDefs,
-    resolvers: {
-        Query: {
-            hello: () => 'world'
-        }
-    }
+    resolvers
 });
 
 const { url } = await startStandaloneServer(server, {
